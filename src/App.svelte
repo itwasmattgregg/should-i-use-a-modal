dd35ba9bb3907bf6a4bffb5d0c018c873cd5f37d
Matt Gregg <mattdgregg@gmail.com>
2022-01-06 22:40:01 -0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Modals, closeModal, openModal } from 'svelte-modals';
+  import Modal from './Modal.svelte';
+
+  function handleClick() {
+    openModal(Modal, { title: 'Alert', message: 'This is an alert' });
+  }
+</script>
+
+<h1>Should I use a modal?</h1>
+<button on:click={handleClick}>Open Modal</button>
+
+<Modals>
+  <div slot="backdrop" class="backdrop" on:click={closeModal} />
+</Modals>
+
+<style>
+  .backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.5);
+  }
+</style>
